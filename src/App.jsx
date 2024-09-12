177c52739de58b458054b64cc7a6adb374cdbc32
Camila Franco <camilafranco512@gmail.com>
2024-09-12 20:00:34 -0300

    Final 1.0

---
+++ src/App.jsx
@@ -0,0 +1,72 @@
+import './App.css';
+import { useState } from 'react';
+import JSConfetti from 'js-confetti';
+
+function App() {
+  const [nombre, setNombre] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  
+
+  const actualizarNombre = (e) => {
+    setNombre(e.target.value);
+  };
+
+  const correctPassword = "123456";
+  const jsConfetti = new JSConfetti();
+
+  const checkPassword = (e) => {
+    e.preventDefault();
+    if (password === correctPassword) {
+      setError("");
+      setBienvenido(true);
+      jsConfetti.addConfetti({
+        emojis: '⚛️',
+        emojiSize: 50
+      });
+    } else {
+      setError("Contraseña incorrecta");
+      setBienvenido(false);
+    }
+  };
+
+  return (
+    <div className='contenido'>
+      <h1>Hola {nombre}</h1>
+      <form  className= 'formulario' onSubmit={(e) => checkPassword(e)}>
+        
+        {/* Usuario */}
+        <div className='centered'>
+          <label htmlFor="usuario">Usuario:</label>
+          <input
+            id='usuario'
+            type="text"
+            required={true}
+            onChange={(e) => actualizarNombre(e)}
+          />
+        </div>
+        {/* Contraseña */}
+        <div className='centered'>
+          <label htmlFor="pass">Contraseña:</label>
+          <input
+            id='pass'
+            type="password"
+            required={true}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        {error && <p style={{ color: 'red' }}>{error}</p>}
+        <button style={{ marginTop: '20px' }}>Entrar</button>
+      </form>
+      {bienvenido && (
+        <div style={{ marginTop: '20px' }}>
+          <h1>Bienvenide {nombre}!</h1>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
+
---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base : "https://camilafranco1103.github.io/reto-05-codeaRock",
+})
