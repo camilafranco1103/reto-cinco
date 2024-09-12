0ec3573ea8a6b2fb167b13820fa0a3cc5386fb2c
Camila Franco <camilafranco512@gmail.com>
2024-09-12 20:22:13 -0300

    final02

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base : "https://camilafranco1103.github.io/reto-05-codeaRock",
+  base : "https://camilafranco1103.github.io/reto-cinco",
 })
